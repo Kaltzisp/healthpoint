01359b3464d736019f25634f50e293f142021667
Peter Kaltzis <peterkaltzis@outlook.com>
2024-07-19 00:12:31 +1000

    fixed ShowGraphs

--- src/Components/ShowGraphs.tsx
+++ src/Components/ShowGraphs.tsx
@@ -1,31 +1,20 @@
 import { type Datum, retrieveData } from "../services/api";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function ShowGraphs(): React.JSX.Element {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [items, setItems] = useState<Datum[]>([]);
 
-    function renderData(data: Datum[]): React.JSX.Element {
-        return (
-            <ul>
-                {
-                    data.map(item => 
-                        <li key={item.key}>{`${item.date}\t${item.dataType}\t${item.value}`}</li>
-                    )
-                }
-            </ul>
-        );
-    }
+    useEffect(() => {
+        retrieveData().then((saveData) => {
+            setItems(saveData);
+        }).catch(e => console.error(e));
+    }, []);
 
     return (
         <div id="exercise-list">
-            {items.length > 0 ? 
-                renderData(items)
-                : (((): void => {
-                    retrieveData().then((data) => {
-                        setItems(data);
-                    }).catch(e => console.error(e));
-                })(), <p>{"Loading..."}</p>)}
+            { items.length > 0 ? 
+                <ul> {items.map(item => <li key={item.key}>{`${item.date}\t${item.dataType}\t${item.value}`}</li>)} </ul>
+                : "Loading..."}
         </div>
     );
 }
